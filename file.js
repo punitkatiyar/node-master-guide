44bc5fe7dd676e251fe3632d4ccc5e5923cd5ffb
Punit Katiyar <punit.kat@gmail.com>
2025-01-21 12:10:57 +0000

    d

---
+++ file.js
@@ -0,0 +1,10 @@
+const fs=require("fs");
+
+// fs.writeFileSync("./test.txt","Welcome to file data");
+
+// fs.writeFileSync("./test.txt","Welcome to file data new");
+
+
+fs.writeFile("./test.txt","This is new file",(err)=>{})
+
+
